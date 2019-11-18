18112226c03264aae09f1438a98642c60f3f5751
limba57 <limba@orange.fr>
2019-11-18 11:02:59 +0100

    Initial commit

---
+++ src/StringAndConcatenation/StringAndConcatenation.java
@@ -0,0 +1,37 @@
+package StringAndConcatenation;
+
+public class StringAndConcatenation {
+
+    public static void main(String[] args) {
+
+        String text = "hello";
+        String antherText = new String("Hello again");
+
+        // exemple
+        System.out.println(2 + 3);       // 5
+        System.out.println("a" + "b");   // ab
+        System.out.println(1 + 2 + "d"); // 3d
+
+        int three = 3;
+        String four = "4";
+        System.out.println(1+2+three+four); // 64
+
+        int number = 10;
+        int antherNumber = 20;
+        System.out.println("result  = " + number + antherNumber);   // result = 1020
+        System.out.println("result  = " + (number + antherNumber)); // result = 30
+
+        String str = "";
+        for (int i = 1; i <= 10; i++) {
+            str += i + " ";
+        }
+        System.out.println(str);
+
+        String hello = "hello";
+        String word = "word";
+        String helloWord = hello + word;
+        System.out.println(hello);
+
+    }
+
+}
---
+++ src/UnderstandImmutability/UnderstandingImmutability.java
@@ -0,0 +1,4 @@
+package UnderstandImmutability;
+
+public class UnderstandingImmutability {
+}
